f86e81f4fe790e06dd0f4b2c27b1ea857ca0f075
Simon Gaudek <simon.gaudek@frauscher.com>
2022-08-04 10:55:07 +0200

    Test for ngOnInit and ngAfterViewInit

--- src/app/b/b.component.ts
+++ src/app/b/b.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-b',
   templateUrl: './b.component.html',
   styleUrls: ['./b.component.css']
 })
-export class BComponent implements OnInit {
+export class BComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    console.log("!!!! ngOnInit called !!!!");
+  }
+
+  public ngAfterViewInit(): void {
+    console.log("!!!! ngAfterViewInit called !!!!");
   }
 
 }
